ebdd8601d6548980f447f091811ec6880b1cd107
aaryanrajputhere <itsaaryanrajput@gmail.com>
2025-11-21 00:27:05 +0530

    MaterialPage Error Fix

--- vendors/src/pages/MaterialsPage.tsx
+++ vendors/src/pages/MaterialsPage.tsx
@@ -1,10 +1,9 @@
 import MaterialsTable from "../components/Materials/MaterialsTable";
 
 export default function MaterialsPage() {
-    
   return (
     <div>
-      <MaterialsTable Materials={[]} />
+      <MaterialsTable />
     </div>
   );
 }
